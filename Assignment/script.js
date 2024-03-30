a1b9dd9c280b72f72f4232c3597096b49572446f
Arundhathy-k <85393433+Arundhathy-k@users.noreply.github.com>
2024-03-30 22:41:42 +0530

    Add files via upload

---
+++ Assignment/script.js
@@ -0,0 +1,48 @@
+var cobtn1 = document.querySelector('.covid-btn1');
+cobtn1.addEventListener('click', () => {
+    cobtn1.style.backgroundColor = "rgb(250, 234, 230) ";
+    cobtn1.style.border = "3px solid rgb(0, 255, 76)";
+})
+
+var cobtn2 = document.querySelector('.covid-btn2');
+cobtn2.addEventListener('click', () => {
+    cobtn2.style.backgroundColor = "rgb(250, 234, 230) ";
+    cobtn2.style.border = "3px solid rgb(0, 255, 76)";
+})
+
+var cobtn3 = document.querySelector('.covid-btn3');
+cobtn3.addEventListener('click', () => {
+    cobtn3.style.backgroundColor = "rgb(250, 234, 230) ";
+    cobtn3.style.border = "3px solid rgb(0, 255, 76)";
+})
+
+var cobtn4 = document.querySelector('.covid-btn4');
+cobtn4.addEventListener('click', () => {
+    cobtn4.style.backgroundColor = "rgb(250, 234, 230) ";
+    cobtn4.style.border = "3px solid rgb(0, 255, 76)";
+})
+
+
+var diabtn1 = document.querySelector('.dia-btn1');
+diabtn1.addEventListener('click', () => {
+    diabtn1.style.backgroundColor = "rgb(250, 234, 230) ";
+    diabtn1.style.border = "3px solid rgb(0, 255, 76)";
+})
+
+var diabtn2 = document.querySelector('.dia-btn2');
+diabtn2.addEventListener('click', () => {
+    diabtn2.style.backgroundColor = "rgb(250, 234, 230) ";
+    diabtn2.style.border = "3px solid rgb(0, 255, 76)";
+})
+
+var diabtn3 = document.querySelector('.dia-btn3');
+diabtn3.addEventListener('click', () => {
+    diabtn3.style.backgroundColor = "rgb(250, 234, 230) ";
+    diabtn3.style.border = "3px solid rgb(0, 255, 76)";
+})
+
+var diabtn4 = document.querySelector('.dia-btn4');
+diabtn4.addEventListener('click', () => {
+    diabtn4.style.backgroundColor = "rgb(250, 234, 230) ";
+    diabtn4.style.border = "3px solid rgb(0, 255, 76)";
+})
